d53f67e40a4f4cc6517caa3ec25029fc972f7502
João Almeida <joaofilipe90@gmail.com>
2025-04-10 15:38:34 +0100

    Update _genonce.sh

--- _genonce.sh
+++ _genonce.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
-publisher_jar=publisher.jar
-input_cache_path=./input-cache/
-echo Checking internet connection...
+
+echo "Checking internet connection..."
 curl -sSf tx.fhir.org > /dev/null
 
 if [ $? -eq 0 ]; then
-	echo "Online"
-	txoption=""
+    echo "Online"
+    txoption=""
 else
-	echo "Offline"
-	txoption="-tx n/a"
+    echo "Offline"
+    txoption="-tx n/a"
 fi
 
 echo "$txoption"
 
-publisher=$input_cache_path/$publisher_jar
-if test -f "$publisher"; then
-	java -jar $publisher -ig . $txoption $*
-
-else
-	publisher=../$publisher_jar
-	if test -f "$publisher"; then
-		java -jar $publisher -ig . $txoption $*
-	else
-		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
-	fi
-fi
+# Run IG Publisher using Docker
+docker run --rm \
+  -v "$(pwd)":/tmp/ig \
+  ghcr.io/trifork/ig-publisher:latest \
+  -ig /tmp/ig $txoption "$@"
